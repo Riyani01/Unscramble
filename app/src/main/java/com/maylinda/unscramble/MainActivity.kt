2ce6fa0258d1358b44665042de0e20b58fc4c37b
MayLinda06 <edihermawan1223@gmail.com>
2022-09-21 18:34:59 +0700

    awal

---
+++ app/src/main/java/com/maylinda/unscramble/MainActivity.kt
@@ -0,0 +1,13 @@
+//TODO 4: Mengarahkan target ke main_activity
+package com.maylinda.unscramble
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
